ff086cb60b253dbf960ba5d18cae7a5eb32ad454
Dan Mateer <dan@pullrequest.com>
2018-01-17 11:12:07 -0600

    changed formatting

--- test_php_file.php
+++ test_php_file.php
@@ -1,10 +1,13 @@
 <?php
-    $greeting ="Hello, Friends\n";
-    $secondary_greeting ="How are you today?\n";
 
-    echo $greeting . "\n";
-    echo $greeting{0} . "\n";
-    $greeting{0} = J;
-    echo $greeting . "\n";
-    echo $secondary_greeting;
+// try this
+$greeting ="Hello, Friends";
+$secondary_greeting ="How are you today?";
+
+echo $greeting . "<br>" . PHP_EOL;
+echo $greeting{0} . "<br>" . PHP_EOL;
+$greeting{0} = J;
+echo $greeting . "<br>" . PHP_EOL;
+echo $secondary_greeting . "<br>" . PHP_EOL;
+
 ?>
